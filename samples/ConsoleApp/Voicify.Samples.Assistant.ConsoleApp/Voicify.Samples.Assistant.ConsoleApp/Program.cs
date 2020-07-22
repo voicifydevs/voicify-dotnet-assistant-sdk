ac39692473f8a511ca4e1696e55bfbcd80cec409
Alex Dunn <alexander.dunn@outlook.com>
2020-07-22 14:44:43 -0400

    setup for assistant console app sample

---
+++ samples/ConsoleApp/Voicify.Samples.Assistant.ConsoleApp/Voicify.Samples.Assistant.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Voicify.Samples.Assistant.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Voicify Assistant Sample App!");
+            Console.WriteLine("Enter your \"Custom Assistant\" fulfillment URL for the app you want to use. You can find it in your app settings in the Voicify CMS");
+            var url = Console.ReadLine();
+
+
+        }
+    }
+}
